3eefcb07fe6a9a9c4bb12c82a2a5e66b0ca80de8
Ekeyangwen <ekeyangwen@gmail.com>
2022-06-02 18:42:41 +0200

    JSON OK + accès données

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,25 +1,45 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useParams, Navigate } from "react-router-dom";
+import { info } from "sass";
 
 const Dashboard = () => {
   const { id } = useParams();
   const [user, setUser] = useState("");
+  const [performance, setPerformance] = useState("");
 
   useEffect(() => {
-    fetch("../../../data.json")
+    fetch("../../../dataInfoUsers.json")
       .then((response) => response.json())
       .then((myJson) => {
         let userFind = myJson.find((user) => user.id === id);
         setUser(userFind);
       });
+    fetch("../../../dataPerformanceUsers.json")
+      .then((response) => response.json())
+      .then((myJson) => {
+        let performanceFind = myJson.find(
+          (performance) => performance.userId === id
+        );
+        setPerformance(performanceFind);
+        console.log(performance);
+      });
   }, []);
+
   console.log(user);
   if (user === undefined) {
     return <Navigate to="*" />;
   }
 
-  return <div>Bonjour {user.userInfos.firstName}</div>;
+  const infoPerformance = performance.kind;
+  const infoUser = user.userInfos;
+
+  console.log(infoPerformance);
+  return (
+    <div>
+      Bonjour {user && infoUser} {user && infoPerformance}
+    </div>
+  );
 };
 
 export default Dashboard;
